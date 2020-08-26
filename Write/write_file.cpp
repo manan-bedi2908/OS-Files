ded0fb61f9f6387bf1d8f4f9730c4bdf83b1ecfa
manan-bedi2908 <mananbedilps@gmail.com>
2020-08-26 19:29:45 +0530

    added

--- Write/write_file.cpp
+++ Write/write_file.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
 #include <fstream>
- 
+
 using namespace std;
- 
 int main()
 {
    fstream file; 							//object of fstream class
    string fileName;
    int age;
+   int choice;
    string placeName;
+   cout<<"\n1. Overwrite\n2.Append\n";
+   cin>>choice;
    cout<<"\nEnter the file name in which you want to write: ";
    cin>>fileName;
-   file.open(fileName,ios::out);
-    
-   if(!file)
-   {
-       cout<<"Error in creating file!!!"<<endl;
-       return 0;
+   if (choice==1) {
+     file.open(fileName,ios::out);
+     if(!file)
+     {
+         cout<<"Error in creating file!!!"<<endl;
+         return 0;
+     }
+
+     cout<<"File created successfully."<<endl;
+     //write text into file
+     cout <<"Writing to a text file:" << endl;
+     cout << "Please Enter your age: ";
+     cin>>age;
+     file << "\n"<<age << endl;
+     cin.ignore();
+     cout<<"\nWhere do you live: ";
+     cin>>placeName;
+     file<<placeName;
+     cout<<endl;
+     cin.ignore();
+     file.close();
+     //closing the file
    }
-    
-   cout<<"File created successfully."<<endl;
-   //write text into file
-   cout <<"Writing to a text file:" << endl;  
-   cout << "Please Enter your age: ";  
-   cin>>age;  
-   file << age << endl; 
-   cin.ignore();
-   cout<<"\nWhere do you live: ";   
-   cin>>placeName;
-   file << placeName;
-   cin.ignore();
-   file.close();  
-   //closing the file
-}
+   else {
+     file.open(fileName, ios::app);
+     if(!file)
+     {
+         cout<<"Error in creating file!!!"<<endl;
+         return 0;
+     }
+
+     cout<<"File created successfully."<<endl;
+     //write text into file
+     cout <<"Writing to a text file:" << endl;
+     cout << "Please Enter your age: ";
+     cin>>age;
+     file << "\n"<<age << endl;
+     cin.ignore();
+     cout<<"\nWhere do you live: ";
+     cin>>placeName;
+     file<<placeName;
+     cout<<endl;
+     cin.ignore();
+     file.close();
+     //closing the file
+   }
+   return 0;
+ }
